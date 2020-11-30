0784ab48613070813f9ae62303eaa112e9927b51
Bim Bo <yousef.yas0080@gmail.com>
2020-11-30 09:51:20 +0330

    Tag model compeleted

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -4,8 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+use Carbon\Carbon;
 
 class Tag extends Model
 {
     use HasFactory;
+    protected $guarded = [];
+
+    // Relations
+    public function setSlugAttribute()
+    {
+        $uniqueNum = substr(Carbon::now()->getPreciseTimestamp(4),-7);
+        $this->attributes['slug'] = Str::slug($this->attributes['title'])."-".$uniqueNum;
+    }
+
+    public function posts()
+    {
+        return $this->belongsToMany(Post::class);
+    }
+
 }
