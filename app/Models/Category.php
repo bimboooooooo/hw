dab02603b2f0f3589244da8ca0bdc4a2ad8e5f2a
Bim Bo <yousef.yas0080@gmail.com>
2020-11-30 09:53:44 +0330

    Category model compeleted

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     use HasFactory;
+
+    protected $guarded = [];
+    public $timestamps = false;
+
+    //Relations
+    public function posts()
+    {
+        return $this->belongsToMany(Post::class);
+    }
+
+    public function image()
+    {
+        return $this->morphOne(Image::class,'imageable');
+    }
 }
