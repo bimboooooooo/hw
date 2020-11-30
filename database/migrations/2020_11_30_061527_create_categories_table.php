f44706b8d7b379065c33ac00fcbda1bfd0ec5e6e
Bim Bo <yousef.yas0080@gmail.com>
2020-11-30 09:57:55 +0330

    Category migration compeleted

--- database/migrations/2020_11_30_061527_create_categories_table.php
+++ database/migrations/2020_11_30_061527_create_categories_table.php
@@ -6,24 +6,16 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateCategoriesTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
+
     public function up()
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('title');
+            $table->string('slug');
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down()
     {
         Schema::dropIfExists('categories');
