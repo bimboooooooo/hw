d4b0f32375cb604f58f84adacf6e4a5d0f6e9178
Bim Bo <yousef.yas0080@gmail.com>
2020-11-30 09:50:37 +0330

    Post model compeleted

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,9 +3,40 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+use Carbon\Carbon;
 
 class Post extends Model
 {
-    use HasFactory;
+    use HasFactory,softDeletes;
+    protected $guarded = [];
+
+    // Mutators
+    public function setSlugAttribute()
+    {
+        $uniqueNum = substr(Carbon::now()->getPreciseTimestamp(4),-7);
+        $this->attributes['slug'] = Str::slug($this->attributes['title'])."-".$uniqueNum;
+    }
+    // Relations
+    public function author()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class);
+    }
+
+    public function categories()
+    {
+        return $this->belongsToMany(Category::class);
+    }
+
+    public function image()
+    {
+        return $this->morphOne(Image::class,'imageable');
+    }
 }
