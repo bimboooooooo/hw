aac642fadafd1b87f90599f97640e32073270b6f
Bim Bo <yousef.yas0080@gmail.com>
2020-11-30 09:57:32 +0330

    Tag migration compeleted

--- database/migrations/2020_11_30_061520_create_tags_table.php
+++ database/migrations/2020_11_30_061520_create_tags_table.php
@@ -6,24 +6,16 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateTagsTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
+
     public function up()
     {
         Schema::create('tags', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('title');
+            $table->string('slug');
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down()
     {
         Schema::dropIfExists('tags');
