c88567a1ef6d2f03e07337a082837b869e73589a
Bim Bo <yousef.yas0080@gmail.com>
2020-11-30 09:53:06 +0330

    Image model compeleted

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class Image extends Model
 {
     use HasFactory;
+
+    protected $guarded = [];
+    public $timestamps = false;
+
+    //Alternative text setter
+    public function setAltAttribute($value)
+    {
+        $this->attributes['alt'] = $value ?? $this->attributes['title'];
+    }
+
+    // Relations
+    public function imageable()
+    {
+        return $this->morphTo();
+    }
 }
