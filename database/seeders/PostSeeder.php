3a2b6c4cbb89ef63cfdfe11f3f2af9fdc0d51985
Bim Bo <yousef.yas0080@gmail.com>
2020-12-01 12:22:12 +0330

    Compeleted Post seeder

--- database/seeders/PostSeeder.php
+++ database/seeders/PostSeeder.php
@@ -3,16 +3,19 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\Post;
+use App\Models\Tag;
+use App\Models\Category;
 
 class PostSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
+
     public function run()
     {
-        //
+        Post::factory()
+            ->count(2000)
+            ->has(Tag::factory()->count(rand(3,10)))
+            ->has(Category::factory()->count(rand(1,3)))
+            ->create();
     }
 }
