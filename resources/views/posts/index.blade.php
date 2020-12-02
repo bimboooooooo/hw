f42bdd2b873d363241c6128f9b103f156093a79b
Bim Bo <yousef.yas0080@gmail.com>
2020-12-02 13:39:41 +0330

    fixed controller and Route

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -5,37 +5,44 @@
 @endpush
 
 @section('content')
-    <table class="table table-dark">
-        <thead>
-        <tr>
-            <th scope="col">ID</th>
-            <th scope="col">Title</th>
-            <th scope="col">Slug</th>
-            <th scope="col">Author</th>
-            <th scope="col">Categories</th>
-            <th scope="col">Tags</th>
-            <th scope="col">Actions</th>
-        </tr>
-        </thead>
-        <tbody>
-        @foreach($posts as $post)
+    <div class="container">
+        <table class="table table-dark">
+            <thead>
             <tr>
-                <th scope="row">{{$post->id}}</th>
+                <th scope="col">ID</th>
+                <th scope="col">Title</th>
+                <th scope="col">Slug</th>
+                <th scope="col">Author</th>
+                <th scope="col">Categories</th>
+                <th scope="col">Tags</th>
+                <th scope="col">Actions</th>
+            </tr>
+            </thead>
+            <tbody>
+                    @foreach($posts as $post)
+            <tr>
+                <th scope="row">{{$post->id }}</th>
                 <td>{{ $post->title }}</td>
                 <td>{{ $post->slug }}</td>
                 <td>{{ $post->athor }}</td>
                 <td>{{ $post->category }}</td>
                 <td>{{ $post->tag }}</td>
                 <td>
+                    <form class="btn btn-outline-success" action="" method="post">
+                        @csrf
+                        Show
+                    </form>
                     <form class="btn btn-danger" action="" method="post">
-{{--                    <button class="btn btn-danger">Delete</button>--}}
+                        @csrf
+                        Delete
                     </form>
                 </td>
                 <td></td>
             </tr>
-        @endforeach
-        </tbody>
-    </table>
+                    @endforeach
+            </tbody>
+        </table>
+    </div>
 @endsection
 
 @push('scripts')
